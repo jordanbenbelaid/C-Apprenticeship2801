fdc3d713a057edc4314791b14565752a11aaeba8
Jordan <jordanbenbelaid@hotmail.com>
2025-01-28 15:56:02 +0000

    Solution for the generics exercise

---
+++ GenericExerciseSolution/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace GenericExerciseSolution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Repository<int> intRepo = new Repository<int>();
+            intRepo.Add(1);
+            intRepo.Add(2);
+            intRepo.Add(3);
+            Console.WriteLine("Integer Repository:");
+            foreach (var item in intRepo.GetAll())
+            {
+                Console.WriteLine(item);
+            }
+
+            // Example with strings
+            Repository<string> stringRepo = new Repository<string>();
+            stringRepo.Add("Hello");
+            stringRepo.Add("World");
+
+            Console.WriteLine("\nString Repository:");
+            foreach (var item in stringRepo.GetAll())
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ GenericExerciseSolution/Repository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericExerciseSolution
+{
+    public class Repository<T>
+    {
+        private List<T> items = new List<T>();
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+
+        public List<T> GetAll()
+        {
+            return items;
+        }
+    }
+}
