c1aec13f920731b2df8df3bc6b38f17571418d61
Jordan <jordanbenbelaid@hotmail.com>
2025-01-28 14:11:32 +0000

    Enum demo upload from the code along

---
+++ EnumDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace EnumDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder("This is a string");
+
+
+
+
+
+
+            //Enums example demo
+            //Console.WriteLine("Enter a direction (North, East, South or West)");
+            //string input = Console.ReadLine();
+
+            //if (Enum.TryParse(input, out Direction direction))
+            //{
+            //    switch(direction)
+            //    {
+            //        case Direction.North:
+            //            Console.WriteLine("You are travelling North");
+            //            break;
+            //        case Direction.East:
+            //            Console.WriteLine("You are travelling East");
+            //            break;
+            //        case Direction.South:
+            //            Console.WriteLine("You are travelling South");
+            //            break;
+            //        case Direction.West:
+            //            Console.WriteLine("You are travelling West");
+            //            break;
+            //    }
+            //} else
+            //{
+            //    Console.WriteLine("Oh no, you got lost, try again");
+            //}
+        }
+    }
+}
