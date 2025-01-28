823dc42a0063123da21ff467acd07d868aba34ef
Jordan <jordanbenbelaid@hotmail.com>
2025-01-28 14:30:14 +0000

    Enum exercise solution

---
+++ EnumExerciseSolution/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace EnumExerciseSolution
+{
+    public class Program
+
+    {
+        static void Main(string[] args)
+        {
+            // Assigning an enum value
+            DaysOfWeek Day = DaysOfWeek.Monday;
+            Console.WriteLine(Day);
+
+            // Enum to integer conversion
+            int numericValue = (int)Day;
+            Console.WriteLine($"Numeric value of {Day} is: {numericValue}");
+
+            // Integer to enum conversion
+            DaysOfWeek dayFromNumber = (DaysOfWeek)3;
+            Console.WriteLine($"The day represented by 3 is: {dayFromNumber}");
+
+            // Using enum in a switch statement
+            switch (Day)
+            {
+                case DaysOfWeek.Monday:
+                    Console.WriteLine("Start of the workweek.");
+                    break;
+                case DaysOfWeek.Friday:
+                    Console.WriteLine("Almost the weekend!");
+                    break;
+                default:
+                    Console.WriteLine("Just another day.");
+                    break;
+            }
+        }
+    }
+}
