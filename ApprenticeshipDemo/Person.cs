767713c32e83c25ee7e2587d583cdf06b7f799db
Jordan <jordanbenbelaid@hotmail.com>
2025-01-28 08:40:01 +0000

    Day 1 C#

---
+++ ApprenticeshipDemo/Person.cs
@@ -0,0 +1,47 @@
+﻿namespace ApprenticeshipDemo
+{
+    public class Person
+    {
+        //attributes of a person
+        private int Age;
+        private string Name;
+        private string Email;
+
+        //constructor
+        public Person(int Age, string Name, string Email)
+        {
+            this.Age = Age;
+            this.Name = Name;
+            this.Email = Email;
+        }
+
+        public Person() { }
+
+
+        //getters and setters
+
+        public int GetAge()
+        {
+            return this.Age;
+        }
+
+        public void SetAge(int Age)
+        {
+            this.Age = Age;
+        }
+
+        //methods
+
+        public string Walk()
+        {
+            return "I am walking";
+        }
+
+        public override string ToString()
+        {
+            return "Person - Name: " + Name + ", Age: " + Age + ", Email: " + Email;
+        }
+
+
+    }
+}
---
+++ ApprenticeshipDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ApprenticeshipDemo;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Person Jordan = new Person(27, "Jordan", "Jordan@hotmail.com");
+
+        Console.WriteLine(Jordan.Walk());
+
+        Console.WriteLine(Jordan.GetAge());
+
+        Jordan.SetAge(25);
+
+        Console.WriteLine(Jordan.GetAge());
+
+        Console.WriteLine(Jordan);
+
+    }
+
+
+
+}
