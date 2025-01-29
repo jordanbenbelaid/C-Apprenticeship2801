2bb9e24dc44b748e99f4e3fb50c3a6c23492f527
Jordan <jordanbenbelaid@hotmail.com>
2025-01-29 12:53:54 +0000

    Start of inheritance looking at constructors and toString, as well as accessing attributes from the Parent class in the Child class

---
+++ ZooDemoSolution/Dog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooDemoSolution
+{
+    public class Dog : Animal
+    {
+        private string Name;
+
+        public string Name1 { get => Name; set => Name = value; }
+
+        public Dog(int id, string colour, bool hasFur, int noOfLegs, string name)
+            : base(id, colour, hasFur, noOfLegs)
+        {
+            this.Name1 = name;
+        }
+
+        public Dog(string name)
+        {
+            this.Name1 = name;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Dog: \nID: {Id1}, \nColour: {Colour1}, \nDoes it have fur?: {HasFur1}, " +
+                $"\nNumber of legs: {NoOfLegs1}, \nDog's name: {Name1} ";
+        }
+    }
+}
---
+++ ZooDemoSolution/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ZooDemoSolution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal lion = new Animal(1, "orangey brown", true, 4);
+
+            //using the generated getters and setters, we use the generated name, if we want to get,
+            //we use the name of the object we created with the generated method name
+            //Console.WriteLine(lion.Colour1);
+            //lion.Colour1 = "Brown";
+
+            Dog dog = new Dog(2, "Black", true, 4, "Saffron");
+
+            Console.WriteLine(dog);
+
+
+
+            //Encapsulate fields (and use properties):
+            //We access fields through properties (The generated getters and setters)
+            //allows logic (validation etc) in the getters and setters
+            //Flexible as it allows us to easily change implementation without breaking code
+            //Can be a little slower due to the way it calls methods
+
+            //Encapsulate fields (but using fields):
+            //we access directly from fields or getter and setter methods
+            //We have less control over how data is modified/accessed
+            //harder to change data access (manual refactoring)
+            //slightly faster, since its direct access
+
+            
+        }
+    }
+}
